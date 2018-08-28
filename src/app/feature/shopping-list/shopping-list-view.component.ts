ea7db975fde19b8d38a8c352d6df6ee4b42d621e
iamleka <len.karpisek@gmail.com>
2018-08-28 15:58:46 +0200

    add animation to shoppinglistview

--- src/app/feature/shopping-list/shopping-list-view.component.ts
+++ src/app/feature/shopping-list/shopping-list-view.component.ts
@@ -1,27 +1,32 @@
 import { Component, Input } from '@angular/core';
 
 import { ShoppingList, StockService } from '../../core/stock.service';
+import { openclose } from '../../shared/animations';
 
 
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list-view.component.html',
-  styleUrls: ['./shopping-list-view.component.scss']
+  styleUrls: ['./shopping-list-view.component.scss'],
+  animations: [openclose]
 })
 export class ShoppingListViewComponent {
+  submenustate: String = 'closed';
 
-@Input() shoppingList: ShoppingList;
-showList = false;
+  @Input() shoppingList: ShoppingList;
+  showList = false;
 
-constructor(
-  private stockService: StockService
-) {
-}
-markAsBought(id: number, amount: number) {
-  this.stockService.addToStock(id, amount);
-}
-toggle(): void {
-  this.showList = !this.showList;
-}
+  constructor(
+    private stockService: StockService
+  ) {
+  }
+  markAsBought(id: number, amount: number) {
+    this.stockService.addToStock(id, amount);
+  }
+  toggle(): void {
+    this.showList = !this.showList;
+    this.submenustate = this.submenustate === 'open' ? 'closed' : 'open';
+
+  }
 }
